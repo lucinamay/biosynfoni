20def2cdf40d09d16fa81eabae73d40bda71c369
Lucina-May Nollen <lucina-may@Lucina-Mays-MacBook-Pro.local>
2023-09-08 10:02:40 +0200

    organised, usable  codes

---
+++ cOnVERTure.py
@@ -0,0 +1,228 @@
+"""
+
+||||||||||||  ㅇㅅㅇ  |||||||||||||||
+____________________________________
+
+title: cOnVERTure (cdk_rdk_parser)  |
+language: python                    |
+author: Lucina-May Nollen           | 
+institute: WUR Bioinformatics       |
+student no: 1197142                 |
+____________________________________
+
+||||||||||||  ()()()  |||||||||||||||
+
+description:    parses (COCONUT) CDK-style sdf's for its CNP-ID, SMILEs,
+                InChi and molecular formula, returning a sdf of mol objects
+                and csv with related info for further use in fingerprinter 
+                program 
+
+style:          attempting to follow PEP8 styleguide
+
+"""
+#--------------------------------- IMPORTS-------------------------------------
+
+from rdkit import Chem
+from rdkit.Chem import AllChem, Draw
+from rdkit import RDLogger  #for muting warnings
+from sys import argv
+from datetime import date
+import os
+
+#================================ FUNCTIONS ===================================
+#================================== input =====================================
+
+def readr(filename): #takes 9 sec for full file(24764158 line)
+    """
+    reads in file in ls of lines (stripped)
+    input: (str) filename -- file to be read
+           (None / int) len_stop -- stop value for shorter reading
+    """
+    i = 0
+    #dev function:
+    #len_stop = None
+    all_lines = []
+    with open (filename,'r') as sdf:
+        for line in sdf:
+            all_lines.append(line.strip())
+    return all_lines
+
+def entry_parser(lines): #takes 19 secs for 59k entries
+    entries = []
+    current_entry = []
+    for line in lines:
+        current_entry.append(line)
+        if line == '$$$$':
+            entries.append(current_entry)
+            current_entry = []  #new entry
+    return entries
+
+#================================ converter ====================================
+
+def propertifier(sdf_entries): #22 sec
+    """lst of properties in entry as follows:
+    every entry is [[coconut_id],[inchi],[smiles],[molecular_formula],[NPLS]]
+    #for later:     [molecular weight], [name],]
+    """
+    entries_properties = []
+    for entry in sdf_entries:
+        #print(entry)
+        i = 0
+        coconut_id, inchi,smiles, molecular_formula = '','','',''
+        NPLS = ''
+        for i in range(len(entry)):
+            if entry[i] == '> <coconut_id>':
+                coconut_id = entry[i+1]
+            elif entry[i] == '> <inchi>':
+                inchi = entry[i+1]
+            elif entry[i]== '> <SMILES>':
+                smiles = entry[i+1]
+            elif entry[i]== '> <molecular_formula>':
+                molecular_formula = entry[i+1]
+            elif entry[i] == '> <NPL_score>':
+                NPLS = entry[i+1]
+        entries_properties.append([coconut_id, inchi,\
+                                   smiles, molecular_formula, NPLS])
+
+    return entries_properties
+
+def molifier(NP_property_list, representation_info = False, stats_out = True):
+    """ makes mol class out of the NPs from database, using InChi formula,
+    if InChi formula is not correct, it tries the SMILES. Keeps track of errors
+    in InChi and SMILES, by adding the error-causing entries to the respective
+    error-collection
+    input:  (list) NP_property_list -- list of entries where
+                    [0] CNP-ID
+                    [1] InChi
+                    [2] SMILES
+                    [3] molecular formula
+                    [4] NPLS (NaPLeS)
+            (bool) stats_out = True -- yes or no return stats
+
+    returns:    (list) all_mols -- mol classes of all successful inchi/smiles
+                (list) prop_list -- 
+                    [0] CNP-ID 
+                    [1] molecular formula 
+                    [2] NPLS
+                (list) stats -- if set to true, returns
+                    [0] (str) -- entry CNP causing InChi2mol errors
+                    [1] (str) -- entry CNP causing SMILES2mol errors
+                    [2] (list) -- entries causing errors in both
+                    [3] (int)  -- total number of entries
+    """
+    inchiNone = []
+    smilesNone = []
+    bothNone = []
+    all_mols = []
+    prop_list = []
+
+    for entry in NP_property_list:
+        smiles_mol = Chem.MolFromSmiles(entry[2])
+        inchi_mol = Chem.MolFromInchi(entry[1])
+        if inchi_mol is not None:
+            all_mols.append(inchi_mol)
+            if not representation_info:
+                prop_list.append([entry[0]]+entry[3:]) 
+                #avoiding faulty represent.
+            elif representation_info:
+                prop_list.append(entry)
+        elif smiles_mol is not None:
+            all_mols.append(smiles_mol)
+            if not representation_info:
+                prop_list.append([entry[0]]+entry[3:]) 
+                #avoiding faulty represent.
+            elif representation_info:
+                prop_list.append(entry)
+        else: #these do not end up in the mol collection
+            print('{} not successfully molified'.format(entry[0]))
+        #stats
+        if stats_out:
+            if inchi_mol is None:
+                inchiNone.append(entry[0])
+            if smiles_mol is None:
+                smilesNone.append(entry[0])
+            if (smiles_mol is None) and (inchi_mol is None):
+                bothNone.append(entry[0])
+    print('done with molifying  all entries')
+    #output (yes or no stats)
+    if stats_out:
+        stats = [inchiNone, smilesNone, bothNone, len(NP_property_list)]
+        return all_mols, prop_list, stats 
+    else: 
+        return all_mols, prop_list 
+
+#================================= output =====================================
+
+def sdf_writr(mols, outfile):
+    """writes sdf of mols"""
+    writer = Chem.SDWriter(outfile)
+    for mol in mols:
+        writer.write(mol)
+
+def csv_writr(lst, outfile, sep = ","):
+    """writes csv of list of lists
+    """
+    with open(outfile, 'w') as of:
+        for np in lst:
+            if isinstance(np, list):
+                of.write(sep.join(np))
+            elif isinstance(np,str):
+                of.write(np)
+            of.write('\n')
+    return None
+
+def outfile_namer(filename_root: str, addition = '') -> str:
+    """gives outfile names in the right formats (with the date 'MMDD_' prefix)
+    input: (str) filename_root -- main name (e.g. the cdk sdf  filename)
+    requires datetime -- 'from datetime import date' 
+    """
+    today = date.today()
+    my_date = today.strftime("%m%d")
+    
+    if addition:
+        outfile_name = "{}_{}_{}".format(my_date, filename_root, addition)
+    else: 
+        outfile_name = "{}_{}".format(my_date, filename_root)
+
+    return outfile_name
+
+def output_direr(dirname='./output'):
+    """moves to the right directory, if dir doesn't exist yet, will make dir
+    (default uses relative location!: './output')
+    """
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)
+    os.chdir(dirname)
+    return None
+#++++++++++++++++++++++++++++++++++ main ++++++++++++++++++++++++++++++++++++++
+
+def main():
+    #input
+    cdk_sdf = argv[1]
+
+    print('\n', 10*'=','\n', 'cOnVERTURE', '\n',10*'=','\n')
+    #handling
+    lines = readr(cdk_sdf)
+    sdf_entries = entry_parser(lines)
+    properties = propertifier(sdf_entries)
+    all_mols, ids_props, stats = molifier(
+        properties,representation_info = True) 
+    
+    #output
+    actual_name = cdk_sdf.strip('.sdf').split('/')[-1]
+    outfile_name = outfile_namer(actual_name) #str
+
+    out_rdk_sdf_name = ''.join(outfile_name + '_rdk.sdf')
+    out_info_name = ''.join(outfile_name + '_info.tsv')
+    out_stats_name = ''.join(outfile_name + '_stats.csv')
+    
+    output_direr('./output')
+    
+    #sdf_writr(all_mols, out_rdk_sdf_name)
+    csv_writr(ids_props, out_info_name, sep= "\t")
+    csv_writr(stats, out_stats_name)
+    
+    return None
+
+if __name__ == "__main__":
+    main()
---
+++ npc_ifier.py
@@ -0,0 +1,312 @@
+"""
+
+||||||||||||  ㅇㅅㅇ  |||||||||||||||
+____________________________________
+
+title: NPC_ifier                    ||
+language: python                    ||
+author: Lucina-May Nollen           || 
+institute: WUR Bioinformatics       ||
+student no: 1197142                 ||
+____________________________________
+
+||||||||||||  ()()()  |||||||||||||||
+
+description:    using RDK Chem mols and NPClassifier API, requests
+                classification information with time delay
+                saves the classfications every 1000 molecules
+                at the end, takes the 400 files and combines them
+                
+                *checks which files are present in the related folder
+                and starts from where it left off (rewriting only the last file
+                in case it was not completed with a 1000 molecules)
+
+                *uses mol to smiles to make sure the smiles is accurate to the
+                mol files used in the rest of the experiment (fp classif.
+
+"""
+
+#/////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/////////////////////////////////////////
+
+#||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+#/////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/////////////////////////////////////////
+
+#--------------------------------- IMPORTS-------------------------------------
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit import RDLogger  #for muting warnings
+from sys import argv
+from datetime import date
+
+import requests, time, os, json
+#------------------------------------------------------------------------------
+
+#================================= GLOBALS ====================================
+
+#no globals (yet)
+
+#==============================================================================
+
+#================================= CLASSES ====================================
+
+#none yet
+
+#==============================================================================
+
+
+#/////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/////////////////////////////////////////
+
+#================================ FUNCTIONS ===================================
+#================================== input =====================================
+def mols_getr(sdf_file: str, supplier_only = False):
+    suppl = Chem.SDMolSupplier(sdf_file)
+    print('read sdf, number of entries:', len(suppl))
+    start_time = time.time()
+    if supplier_only == False:
+        mols = [x for x in suppl]
+    end_time = time.time()
+    print('mols_getr took {} seconds to finish'.format((end_time - start_time)))
+    for mol in mols:
+        if mol is None:
+            raise Exception(print(mol, 'mol is None; check if you are \
+                    using RDKit-based sdf'))
+    return mols
+
+#================================= handling ===================================
+
+def save_index_getr(rootname: str,save_size= 1000, max_index = 408):
+    """ checks for temporary file lists, returns (list) indexes(int) 
+    still needed in folder
+    filenames are <dateofnpcadd>_<rootname>_npcs_index.tsv
+    """
+    file_list = os.listdir('.')
+    file_roots = [name.strip('.csv').strip('.tsv') for name in file_list]
+    indexes_left = []               #all indexes that are missing
+
+    for i in range(max_index+1):    #includes last index
+        #reset per  index
+        found = False
+
+        #go through list until index found
+        for fi in file_roots:
+            if rootname in fi:
+                if (fi.split('_')[-3:] == [str(save_size),'npcs', str(i)]):
+                    found = True
+                    break
+        if found == True:
+            continue                #skip to next index
+        else:
+            indexes_left.append(int(i))
+    return indexes_left
+
+def slicer(full_lst: list, slice_ind: int, slice_size: int):
+    #num_slices = (total // slice_size) + 1
+
+    i = slice_ind
+    slice_start, slice_end = i*slice_size, (i*slice_size + slice_size)
+
+    return full_lst[slice_start, slice_end]
+
+
+#--------------------------- get classification -------------------------------
+
+def npc_resultr(smiles_string: str):
+    url = "https://npclassifier.ucsd.edu"
+
+    r = requests.get(f"{url}/classify?smiles={smiles_string}")
+    npc_pathway = r.json()['pathway_results']
+    npc_superclass = r.json()['superclass_results']
+    npc_class = r.json()['class_results']
+    return [npc_pathway, npc_superclass, npc_class]
+
+def npc_apier(mol_list: list, index_base = 0,\
+        chunk_size = 5, sleep_time = 5):
+    print('npc_apier running starting at index {}'.format(index_base))
+
+    result = []         #array with rows == molecule
+    rest_count = 0
+    
+    for i in range(len(mol_list)):
+        if rest_count == chunk_size:
+            print('at {}, now sleep'.format(i+index_base))
+            time.sleep(sleep_time)
+            rest_count = 0 
+        mol = mol_list[i]
+        smiles = Chem.MolToSmiles(mol) #gives error if there is a # or +
+        try:
+            npc_result = npc_resultr(smiles)
+            print('.', end='')
+        except:
+            print('e{}'.format(i), end='')
+            errorfile = outfile_namer('api_errors')
+            with open(errorfile, 'a') as errors:
+                errors.write('\t'.join(str(j) for j in [index_base+i,smiles,'\n']))
+            npc_result = [[],[],[]]
+        result.append(npc_result)
+        
+        rest_count +=1
+
+    return result
+
+#==============================================================================
+
+#================================= output =====================================
+
+def outfile_namer(filename_root: str, addition=''):
+    """gives outfile names in the right formats (no extension names)
+    """
+    today = date.today()
+    my_date = today.strftime("%m%d")
+    if len(addition)> 0:
+        outfile_name = "{}_{}_{}".format(my_date, filename_root,\
+                addition)
+    else:
+        outfile_name = "{}_{}".format(my_date, filename_root)
+
+    return outfile_name
+
+
+def nested_list_tsvr(nested_list, filename, sep_large='\t', sep_small=','):
+    totname = filename+'.tsv'
+    with open(totname, 'w') as new:
+        for row in nested_list:
+            new.write(sep_large.join(sep_small.join(thing) for thing in row))
+            new.write('\n')
+    print('written all {} lines in file: {}'.format(len(nested_list), totname))
+    return None
+
+#==============================================================================
+#============================ handling-output loop ============================
+
+def npc_over_set(mols: list, rootname: str, \
+        savesize=1000, chunksize=5, sleep_time=5):
+    """ divides the api requests over 'save chuncks' (i.e. number of molecules
+    after which the api result is being written) (default: 1000) and request
+    chuncks (i.e. number of requests after which the requests 'sleep' for a 
+    while
+    """
+
+    print('starting to loop over full set of {} molecules'.format(len(mols))) 
+
+    #move to the right folder (hardcoded) ---------------------------------
+    storage = './npc_storage'
+    if not os.path.exists(storage):
+        os.mkdir(storage)
+    os.chdir(storage)
+
+    #save-chunk: every <savesize> molecules (default 1000) ----------------
+    num_of_files = (len(mols)//savesize) + 1
+    print(num_of_files, 'should be created in total')
+
+    indexes_left = save_index_getr(rootname,savesize, num_of_files) 
+                            #what savechunck
+    num_left = len(indexes_left)
+    diff = num_of_files - num_left
+    print(num_left, 'are not yet done')
+    print("[{}{}]{}%".format((diff*'='),(num_left*' '),(diff/num_of_files*100)))
+
+    for i in indexes_left:                      
+        #get slice to save to one savefile (not necessarily start with 0)
+        slice_start, slice_end = i*savesize, (i*savesize + savesize)
+        savechunk = mols[slice_start:slice_end]
+        
+        #request entire slice smartly - - - - - - - - - - - - - - - - - -
+        npc_result = npc_apier(savechunk, index_base = slice_start,\
+                chunk_size=chunksize, sleep_time=sleep_time)
+                #returns nested list:  [ [[],[],[]], [[],[],[]], [[],[],[]] ]
+        
+        #after completing slice, write  - - - - - - - - - - - - - - - - - 
+        name_addition = '{}_npcs_{}'.format(savesize, i)
+        newfile_name = outfile_namer(rootname, name_addition)
+        nested_list_tsvr(npc_result, newfile_name)
+    
+    os.chdir('..') #return
+    return None
+
+#----------------------------- afterloop combining ----------------------------
+def output_combinr(rootname, savesize=1000):
+    """combines all the files together
+    """
+    print('starting output combination ...')
+
+    storage = './npc_storage'
+    os.chdir(storage)
+
+    file_list = os.listdir('.')
+    file_roots = [name.strip('.csv').strip('.tsv') for name in file_list]
+    query = '{}_npcs'.format(savesize)
+
+    new_filename = outfile_namer(rootname, 'npcs') + '.tsv'
+    
+    if os.path.exists(new_filename):
+        raise Exception('\n\n{} exists, delete file first'\
+                .format(new_filename))
+
+    relevant = []
+    for fi in file_list:
+        if ((rootname in fi) and (query in fi)):
+            relevant.append(fi)
+    
+    relevant_files = sorted(relevant, key=lambda x: int(x.split('_')[-1]))
+    indexes_done=[]
+
+    for eachfile in relevant_files:               
+            index = eachfile.strip('.tsv').split('_')[-1]
+            if index in indexes_done:
+                continue
+            indexes_done.append(index)
+            print('copying {} into {}'.format(eachfile, new_filename))
+            temp = []
+            #read in small file 
+            with open(eachfile) as readfile:
+                for line in readfile:
+                    temp.append(line.strip())
+            #write to big file
+            with open(new_filename, 'a') as total:
+                for line in temp:
+                    total.write(line)
+                    total.write('\n')
+            #write log of copy
+            with open(outfile_namer(rootname,'log'),'a') as log:
+                log.write('written {} lines of {} to {}'.format(len(temp),\
+                            fi, new_filename))
+    return None
+
+#==============================================================================
+
+
+
+#/////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/////////////////////////////////////////
+#++++++++++++++++++++++++++++++++++ main ++++++++++++++++++++++++++++++++++++++
+#/////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/////////////////////////////////////////
+
+def main():
+    sdf_file = argv[1] #should be the rdk for better performance
+    inputname = sdf_file.strip('.sdf')
+    savesize = 100
+    
+    print('\n', 10*'=','\n', 'NPC_ifier\n', date.today(),'\n', 10*'=','\n')
+
+    #getmolset 
+    mols = mols_getr(sdf_file, supplier_only = False)
+    print('preview of mols',mols[:10],'\n')
+
+    #output
+    os.chdir('./output')
+    npc_over_set(mols, inputname, savesize=savesize, chunksize=5, sleep_time=5)
+    
+    #output combining
+    output_combinr(inputname, savesize)
+
+    print('\n =======\n DONE~~~\n =======\n')
+
+    return None
+
+if __name__ == "__main__":
+    main()
