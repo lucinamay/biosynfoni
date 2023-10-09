41dc2c0b8c138278d6d8fc95d6a9a3c462a8837e
lucinamay <119406697+lucinamay@users.noreply.github.com>
2023-10-09 15:06:42 +0200

    ADD: moved functions from converture to rdkfnx for modularity

--- src/rdkfnx.py
+++ src/rdkfnx.py
@@ -2,15 +2,27 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Sep 13 14:59:28 2023
+||||||||||||  ㅇㅅㅇ  |||||||||||||||
+____________________________________
 
-@author: lucina-may
+title: RDKit functions              ||
+created: 2023-09-13                 ||
+author: Lucina-May Nollen           || 
+institute: WUR Bioinformatics       ||
+____________________________________
+ 
+||||||||||||  ()()()  |||||||||||||||
+
+contains general reusable functionalities depending on RDKit packages,
+mainly supplier-handling
 """
 from rdkit import Chem
 from rdkit.Chem import AllChem, Draw
-from rdkit import RDLogger  #for muting warnings
+from rdkit import RDLogger  # for muting warnings
 import def_biosynfoni
 
-def sdf_writr(mols:list, outfile:str) -> None:
+
+def sdf_writr(mols: list, outfile: str) -> None:
     """writes sdf of mols, 
     * needs RDKit Chem
     """
@@ -19,48 +31,14 @@ def sdf_writr(mols:list, outfile:str) -> None:
         writer.write(mol)
     return None
 
-def get_supplier(sdf_file:str, supplier_only:bool = True)->list:
+
+def get_supplier(sdf_file: str, supplier_only: bool = True) -> list:
     suppl = Chem.SDMolSupplier(sdf_file)
-    
+
     print('reading sdf, number of entries:', len(suppl))
-    
+
     if supplier_only:
         return suppl
     else:
         mols = [x for x in suppl]
         return mols
-    
-def loop_over_supplier(
-        supplier,
-        fp_version:str='',
-        substructure_set:list=[],
-        subs_smarts:dict = def_biosynfoni.SUBSTRUCTURES,
-        fp_versions:dict[list] = def_biosynfoni.FP_VERSIONS,
-        coverage_info=False):
-    """gets the fingerprint of the entire set"""
-    
-    #pass coverage_info bool to matches-returning option in get_biosynfoni
-    matches=coverage_info
-    print("looping over supplier, coverage info provided:", matches)
-    
-    #main functionality
-    fingerprint_collection = []
-    if matches:
-        coverage_collection = []
-        for mol in supplier:
-            fingerprint, matches=get_biosynfoni(
-                mol,
-                version=fp_version,
-                substructure_set=substructure_set,
-                matches=matches)
-            fingerprint_collection.append(fingerprint)
-            coverage_collection.append(get_coverage(mol, matches))
-    else:
-        for mol in supplier:
-            fingerprint=get_biosynfoni(
-                mol,
-                version=fp_version,
-                substructure_set=substructure_set,
-                matches=matches)
-            fingerprint_collection.append(fingerprint)
-    return fingerprint_collection
