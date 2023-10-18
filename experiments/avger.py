e056f58a8972c065ab927eb7545083b37e938553
lucinamay <119406697+lucinamay@users.noreply.github.com>
2023-10-18 13:40:43 +0200

    IMP: cleaned up code

--- experiments/avger.py
+++ experiments/avger.py
@@ -1,8 +1,8 @@
 from sys import argv
-lines  =[]
-with open(argv[1], 'r') as f:
+
+lines = []
+with open(argv[1], "r") as f:
     for line in f:
         lines.append(float(line.strip()))
 
-print(sum(lines)/len(lines))
-
+print(sum(lines) / len(lines))
