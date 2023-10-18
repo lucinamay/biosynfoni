90a9ddff288562354aa714afaccbd13a4a384b4c
lucinamay <119406697+lucinamay@users.noreply.github.com>
2023-10-18 09:19:04 +0200

    IMP: improved argument parser

--- main.py
+++ main.py
@@ -22,22 +22,25 @@ def main():
         required=True,
         help="path to molecule supplier, or smiles/InChI string if -s or - is specified.",
     )
-    
 
     # optionals --------------------------------------------------------------
     # --output--
     parser.add_argument(
-        "-o", "--output", type=str, required=False, help="Path to output tsv file. If not specified, will use outfile_namer"
+        "-o",
+        "--output",
+        type=str,
+        required=False,
+        help="Path to output tsv file. If not specified, will use outfile_namer",
     )
     # --representation--
     parser.add_argument(
         "-r",
         "--representation",
-        type = str
+        type=str,
         help="specify the input type of the molecule(s). If molsupplier, choose sdf (default)",
         action="store_true",
-        default = "sdf",
-        choices=["sdf","smiles", "inchi"], #"sdfgz", "sdfbz2", "sdfzip"],
+        default="sdf",
+        choices=["sdf", "smiles", "inchi"],  # "sdfgz", "sdfbz2", "sdfzip"],
     )
     # -- coverage --
     parser.add_argument(
@@ -49,14 +52,12 @@ def main():
         help="specify if you want an output of the coverage of the biosynfoni",
     )
 
-
     # flags ------------------------------------------------------------------
     parser.add_argument(
         "--header",
         action="store_true",
         help="Flag to indicate that input file contains header.",
     )
-    
 
 
 args = parser.parse_args()
