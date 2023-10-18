985c60ebba4e7bf347f25161deb4cea775831221
lucinamay <119406697+lucinamay@users.noreply.github.com>
2023-10-18 13:47:12 +0200

    BUG: changed csv output

--- experiments/fp_avger.py
+++ experiments/fp_avger.py
@@ -8,11 +8,12 @@ with open(argv[1], "r") as f:
         lines.append(line.strip().split(","))
 
 arr = np.array(lines, dtype=int)
-print(arr.mean(axis=1))
+mean_fp = arr.mean(axis=1)
+print(mean_fp)
 
 with open("fp_avg.csv", "w") as f:
-    for i in range(len(arr)):
-        f.write(f"{i},{arr[i].mean()}\t")
+    for i in range(len(mean_fp)):
+        f.write(f"{mean_fp[i]}\t")
 
 plt.ioff()
 dataset = np.transpose(arr)
