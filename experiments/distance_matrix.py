39ab72a8ec2624aa618e6a9b052312beb7a729ed
Lucina-May <119406697+lucinamay@users.noreply.github.com>
2023-11-29 09:08:26 +0100

    IMP: improved output naming

--- experiments/distance_matrix.py
+++ experiments/distance_matrix.py
@@ -3,6 +3,7 @@ import sys, os
 import numpy as np
 
 from biosynfoni.fingerprints import counted_tanimoto_sim
+from biosynfoni.inoutput import outfile_namer
 
 def distance_matrix(data: np.array, metric: str = "euclidean") -> np.array:
     """returns distance matrix of array"""
@@ -12,13 +13,14 @@ def distance_matrix(data: np.array, metric: str = "euclidean") -> np.array:
         return np.array([[1.0 - counted_tanimoto_sim(i, j) for j in arr] for i in arr])
     elif metric == "manhattan":
         print("warning: not checked!! check formula for manhattan distance")
-        return np.array([[np.linalg.norm(i - j, ord=1) for j in arr] for i in arr]))
+        return np.array([[np.linalg.norm(i - j, ord=1) for j in arr] for i in arr])
 
 def main():
     filename = sys.argv[1]
+    metric = "tanimoto"
     data = np.loadtxt(filename, delimiter=",", dtype=int)
-    distances = distance_matrix(data, metric="tanimoto")
-    np.savetxt("distances.csv", distances, delimiter=",", fmt="%.8f")
+    distances = distance_matrix(data, metric=metric)
+    np.savetxt(outfile_namer(filename, metric), distances, delimiter=",", fmt="%.8f")
 
 
 if __name__ == "main":
