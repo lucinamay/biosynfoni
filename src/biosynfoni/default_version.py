ebc1c0597bc7b0d352351a9239de9569cbf312d7
lucinamay <119406697+lucinamay@users.noreply.github.com>
2024-01-11 10:49:09 +0100

    NEW: holds default biosynfoni version (last one), overridable with just the str of version

---
+++ src/biosynfoni/default_version.py
@@ -0,0 +1,5 @@
+from biosynfoni.versionfonis import fpVersions
+
+# set default substructure collection here.
+defaultVersion = list(fpVersions.keys())[-1]
+defaultVersion = "full_1103"  # override default version
