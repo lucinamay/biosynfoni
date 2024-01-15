2a7327ef962d400f117817736b0ef537d20f7ab3
lucinamay <119406697+lucinamay@users.noreply.github.com>
2024-01-15 14:47:27 +0100

    ADD: pathway partial

--- src/biosynfoni/subkeys/__init__.py
+++ src/biosynfoni/subkeys/__init__.py
@@ -1,4 +1,9 @@
 from biosynfoni.subkeys.versionfonis import fpVersions
 from biosynfoni.subkeys.biosmartfonis import substructureSmarts
 from biosynfoni.subkeys.default_version import defaultVersion
-from biosynfoni.subkeys.get_version import get_smarts, get_names, get_values
+from biosynfoni.subkeys.get_version import (
+    get_smarts,
+    get_names,
+    get_values,
+    get_pathway,
+)
