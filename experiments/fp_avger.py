078549aa66a0003a151cc987d16dc4ee30ddbe59
lucinamay <119406697+lucinamay@users.noreply.github.com>
2024-01-16 10:37:40 +0100

    MOV: moved functions etc

--- experiments/fp_avger.py
+++ experiments/fp_avger.py
@@ -1,5 +1,5 @@
 import sys, os
-from sys import argv
+import argparse
 
 import numpy as np
 import matplotlib
@@ -15,8 +15,18 @@ sys.path.append(
     os.path.abspath(os.path.join(sys.path[0], os.pardir, "src", "biosynfoni"))
 )
 from biosynfoni.inoutput import outfile_namer
-from biosynfoni.subkeys.get_version import FP_VERSIONS
-from utils.figuremaking import heatmap, annotate_heatmap
+from biosynfoni.subkeys import fpVersions, defaultVersion
+from experiments.utils.figuremaking import heatmap, annotate_heatmap
+from experiments.utils import set_style
+
+
+def cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fingerprintfile_coco", type=str)
+    parser.add_argument("fingerprintfile_zinc", type=str)
+    parser.add_argument("bsf_name", type=str, default=defaultVersion)
+    args = parser.parse_args()
+    return args
 
 
 def fp_stats(fp_arr: np.array, set_name: str) -> np.array:
@@ -63,11 +73,27 @@ def fp_plots(fp_arr, bsf_name):
     return None
 
 
-def heatmap_array(fp_arr: np.array, max_height: int = 30):
+def heatmap_array(
+    fp_arr: np.array,
+    max_height: int = 30,
+    percentages=False,
+    accumulative=True,
+    end_accumulative=False,
+):
     heat_array = np.zeros((max_height, fp_arr.shape[1]))
     for i in range(max_height):
-        countrow = np.count_nonzero(fp_arr > i, axis=0)
+        if accumulative:
+            countrow = np.count_nonzero(fp_arr > i, axis=0)
+        else:
+            if end_accumulative and i == max_height - 1:
+                # for last height, count all remaining values
+                countrow = np.count_nonzero(fp_arr > i, axis=0)
+            else:
+                countrow = np.count_nonzero((fp_arr == i + 1), axis=0)
         heat_array[max_height - 1 - i] = countrow
+
+    if percentages:
+        heat_array = heat_array / fp_arr.shape[0] * 100
     return heat_array.astype(int)
 
 
@@ -76,43 +102,104 @@ def fp_heatmap(
     filename: str,
     subsnames: list = [],
     bsfname: str = "",
+    size: tuple[int] = (10, 6),
+    percentages: bool = False,
+    annotate: bool = False,
+    scheme: str = "Purples",
+    title: str = "Representative substructure count for compound collection",
+    top_acc_array=None,
 ):
+    cbarlab = "number of compounds"
+    if percentages:
+        cbarlab = "percentage of compounds"
+
     print("saving heatmap")
     height = fp_arr.shape[0]
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=size, dpi=500)
     if not subsnames:
         subsnames = [f"subs{i}" for i in range(1, fp_arr.shape[1] + 1)]
+
+    yaxlabels = [(height + 1 - i) for i in range(1, height + 1)]
+    if top_acc_array is not None:
+        yaxlabels[0] = f"≥{height}"
+        im2, cbar2 = heatmap(
+            top_acc_array,
+            # ['>11']+[(height+1-i) for i in range(1, height + 1)],
+            yaxlabels,
+            subsnames,
+            ax=ax,
+            cmap="Greys",
+            # cmap = "PiYG",
+            cbarlabel=f"{cbarlab}, accumulative",
+            cbar_kw={"drawedges": False, "shrink": 0.5, "pad": 0.05, "aspect": 10},
+        )
+
     im, cbar = heatmap(
         fp_arr,
-        [i for i in range(1, height + 1)],
+        # [(height+1-i) for i in range(1, height + 1)],
+        yaxlabels,
         subsnames,
         ax=ax,
-        cmap="YlGn",
-        cbarlabel="number of compounds",
+        cmap=scheme,
+        # cmap = "PiYG",
+        cbarlabel=cbarlab,
+        cbar_kw={"drawedges": False, "shrink": 0.5, "pad": 0.05, "aspect": 10},
     )
+
     # texts = annotate_heatmap(im, valfmt="{x:.1f}")
-    texts = annotate_heatmap(im, valfmt="{x:.0e}")
+    if annotate:
+        texts = annotate_heatmap(im, valfmt="{x:.0f}", size=7)
     # plt.figure(figsize=(10,6))
+    ax.set_xlabel("substructure")
+    ax.set_ylabel("counts", labelpad=10)
+    ax.set_title(title, loc="center", pad=20)
     fig.tight_layout()
     # plt.show()
     # for filename, using collection name is nice
-    plt.savefig(f"{outfile_namer(filename, bsfname)}_heatmap.svg")
+    plt.savefig(f"{outfile_namer(filename, bsfname)}_heatmap.png")
+    return None
+
+
+def over_under_divide(array: np.array, limit: int = 10, percentages: bool = True):
+    full = heatmap_array(
+        array,
+        max_height=limit + 1,
+        percentages=percentages,
+        accumulative=False,
+        end_accumulative=True,
+    )
+    under, over = full.astype(float).copy(), full.astype(float).copy()
+    under[0] = np.nan
+    over[1:] = np.nan
+    return under, over
+
+
+def fp_heatmap_accumulative(fp_arr: np.array, limit: int = 10, *args, **kwargs):
+    under, over = over_under_divide(fp_arr, limit)
+    fp_heatmap(
+        under,
+        *args,
+        top_acc_array=over,
+        **kwargs,
+    )
     return None
 
 
 def main():
     print("hello")
-    fingerprintfile_coco = argv[1]  # natural products
-    fingerprintfile_zinc = argv[2]  # synthetic compounds
+    set_style()
+    args = cli()
+    fingerprintfile_coco = args.fingerprintfile_coco  # natural products
+    fingerprintfile_zinc = args.fingerprintfile_zinc  # synthetic compounds
     coco = np.loadtxt(fingerprintfile_coco, dtype=int, delimiter=",")
     zinc = np.loadtxt(fingerprintfile_zinc, dtype=int, delimiter=",")
     coco_name = fingerprintfile_coco.split("/")[-1].split(".")[0]
     zinc_name = fingerprintfile_zinc.split("/")[-1].split(".")[0]
     # bsf_name_coco = coco_name.replace("_noblock", '').replace(split("_")[-1]
-    if len(argv) > 3:
-        bsf_name = argv[3]
-    if FP_VERSIONS[bsf_name]:
-        substructure_names = FP_VERSIONS[bsf_name]
+
+    bsf_name = args.bsf_name
+    if fpVersions[bsf_name]:
+        substructure_names = fpVersions[bsf_name]
 
     # coco_mean = fp_stats(coco, coco_name)
     # zinc_mean = fp_stats(zinc, zinc_name)
--- experiments/utils/figuremaking.py
+++ experiments/utils/figuremaking.py
@@ -13,6 +13,7 @@ ____________________________________
 
 description: functions for figuremaking
 """
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -110,6 +111,7 @@ def annotate_heatmap(
     valfmt="{x:.2f}",
     textcolors=("black", "white"),
     threshold=None,
+    shift=0,
     **textkw,
 ):
     """
@@ -165,6 +167,71 @@ def annotate_heatmap(
             texts.append(text)
 
     return texts
+
+
+# def add_minus_plus(
+#     im,
+#     data=None,
+#     valfmt="{x:.2f}",
+#     textcolors=("black", "white"),
+#     threshold=None,
+#     shift=0,
+#     **textkw,
+# ):
+#     """
+#     A function to annotate a heatmap.
+
+#     Parameters
+#     ----------
+#     im
+#         The AxesImage to be labeled.
+#     data
+#         Data used to annotate.  If None, the image's data is used.  Optional.
+#     valfmt
+#         The format of the annotations inside the heatmap.  This should either
+#         use the string format method, e.g. "$ {x:.2f}", or be a
+#         `matplotlib.ticker.Formatter`.  Optional.
+#     textcolors
+#         A pair of colors.  The first is used for values below a threshold,
+#         the second for those above.  Optional.
+#     threshold
+#         Value in data units according to which the colors from textcolors are
+#         applied.  If None (the default) uses the middle of the colormap as
+#         separation.  Optional.
+#     **kwargs
+#         All other arguments are forwarded to each call to `text` used to create
+#         the text labels.
+#     """
+
+#     if not isinstance(data, (list, np.ndarray)):
+#         data = im.get_array()
+
+#     # Normalize the threshold to the images color range.
+#     if threshold is not None:
+#         threshold = im.norm(threshold)
+#     else:
+#         threshold = im.norm(data.max()) / 2.0
+
+#     # Set default alignment to center, but allow it to be
+#     # overwritten by textkw.
+#     kw = dict(horizontalalignment="center", verticalalignment="center")
+#     kw.update(textkw)
+
+#     # Get the formatter in case a string is supplied
+#     if isinstance(valfmt, str):
+#         # if + if value > 0, if - if value < 0
+#         valfmt = "+" if valfmt > 0 else "-"
+
+#     # Loop over the data and create a `Text` for each "pixel".
+#     # Change the text's color depending on the data.
+#     texts = []
+#     for i in range(data.shape[0]):
+#         for j in range(data.shape[1]):
+#             kw.update(color=textcolors[int(im.norm(data[i, j]) > threshold)])
+#             text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
+#             texts.append(text)
+
+#     return texts
 
 
 def _set_ax_boxplot_i_colour(ax_boxplot, i, colour, inner_alpha=0.6):
