50f978e98546f5dfe558ed58a10d4780d438d5b8
lucinamay <119406697+lucinamay@users.noreply.github.com>
2024-02-02 16:01:37 +0100

    ADD: svg

--- experiments/fp_avger.py
+++ experiments/fp_avger.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, logging
 import argparse
 
 import numpy as np
@@ -15,9 +15,16 @@ sys.path.append(
     os.path.abspath(os.path.join(sys.path[0], os.pardir, "src", "biosynfoni"))
 )
 from biosynfoni.inoutput import outfile_namer
-from biosynfoni.subkeys import fpVersions, defaultVersion, get_names
-from utils.figuremaking import heatmap, annotate_heatmap
+from biosynfoni.subkeys import fpVersions, defaultVersion, get_names, get_pathway
+from utils.figuremaking import (
+    heatmap,
+    annotate_heatmap,
+    savefig,
+    set_label_colors_from_categories,
+    custom_cmap,
+)
 from utils import set_style
+from utils.colours import colourDict
 
 
 def cli():
@@ -34,24 +41,36 @@ def cli():
         type=str,
         help="name of compound collection for title and filename",
     )
+    parser.add_argument(
+        "-c",
+        "--classes",
+        type=str,
+        required=False,
+        help="path to tsv or csv file of classes, uses first column as class label. pass if you want to also plot classes separately",
+    )
     args = parser.parse_args()
+    args.fingerprints = os.path.abspath(args.fingerprints)
+    args.classes = os.path.abspath(args.classes) if args.classes else None
     return args
 
 
-def fp_stats(fp_arr: np.array, set_name: str) -> np.array:
+def write_stats(fp_arr: np.array) -> np.array:
     mean_fp = fp_arr.mean(axis=0)
     std_fp = fp_arr.std(axis=0)
-    outfile = f"{outfile_namer('fp_avg', set_name)}.csv"
-    with open(outfile, "w") as f:
+    median_fp = np.median(fp_arr, axis=0)
+    with open("stats.tsv", "w") as f:
+        f.write("mean\n")
         f.write("\t".join([str(x) for x in mean_fp]))
-        f.write("\n")
+        f.write("\nstdev\n")
         f.write("\t".join([str(x) for x in std_fp]))
+        f.write("\nmedian\n")
+        f.write("\t".join([str(x) for x in median_fp]))
     return mean_fp
 
 
 def fp_means_plots(fp_mean_arr1, fp_mean_arr2, filename):
     plt.figure()
-    print("making plot")
+    logging.info("making plot")
 
     joined = np.array([fp_mean_arr1, fp_mean_arr2])
     both = np.transpose(np.array([fp_mean_arr1, fp_mean_arr2]))
@@ -64,7 +83,7 @@ def fp_means_plots(fp_mean_arr1, fp_mean_arr2, filename):
     plt.title(
         "mean fp count per substructure for natural products and synthetic compounds"
     )  # , fontsize=20)
-    print("saving plot")
+    logging.info("saving plot")
     plt.legend()
     plt.savefig(filename)
     plt.close()
@@ -74,9 +93,9 @@ def fp_means_plots(fp_mean_arr1, fp_mean_arr2, filename):
 def fp_plots(fps, bsf_name):
     plt.ioff()
     plt.figure().set_figwidth(15)
-    print("making plot")
+    logging.info("making plot")
     plt.violinplot(dataset=fps, showmeans=True)
-    print("saving plot")
+    logging.info("saving plot")
     plt.savefig(f"fp_avg_{bsf_name}.svg")
     plt.close()
     return None
@@ -115,31 +134,44 @@ def fp_heatmap(
     color_scheme: str = "Purples",
     title: str = "Representative substructure count for compound collection",
     top_acc_array=None,
+    standard_colour: bool = False,
 ):
     cbarlab = "number of compounds"
     if percentages:
-        cbarlab = "percentage of compounds"
+        cbarlab = "% of compounds"
 
-    print("saving heatmap")
+    logging.info("saving heatmap")
     height = fp_hm_array.shape[0]
     fig, ax = plt.subplots(figsize=size, dpi=500)
     if not subslabels:
         subslabels = [f"subs{i}" for i in range(1, fp_hm_array.shape[1] + 1)]
+    subslabels = [x.replace("_", " ") for x in subslabels]
 
     yaxlabels = [(height + 1 - i) for i in range(1, height + 1)]
     if top_acc_array is not None:
         yaxlabels[0] = f"≥{height}"
+        maxtop = top_acc_array[~np.isnan(top_acc_array)].max()
+        maxfp = fp_hm_array[~np.isnan(fp_hm_array)].max()
+        maxval = max(maxtop, maxfp)
         im2, cbar2 = heatmap(
             top_acc_array,
             # ['>11']+[(height+1-i) for i in range(1, height + 1)],
             yaxlabels,
             subslabels,
             ax=ax,
-            cmap="Greys",
+            cmap=custom_cmap("Greys", first_color="#ffffff00"),
             # cmap = "PiYG",
-            cbarlabel=f"{cbarlab}, accumulative",
-            cbar_kw={"drawedges": False, "shrink": 0.5, "pad": 0.05, "aspect": 10},
+            cbar_kw={
+                "drawedges": False,
+                "shrink": 0.3,
+                "pad": -0.05,
+                "aspect": 10,
+            },
+            vmin=0,
+            vmax=maxval,
         )
+        # rotate cbar labels -90
+        cbar2.set_label(f"{cbarlab} ≥{height}", rotation=90, va="bottom", labelpad=10)
 
     im, cbar = heatmap(
         fp_hm_array,
@@ -147,24 +179,29 @@ def fp_heatmap(
         yaxlabels,
         subslabels,
         ax=ax,
-        cmap=color_scheme,
+        cmap=custom_cmap(color_scheme, first_color="#ffffff00"),
         # cmap = "PiYG",
         cbarlabel=cbarlab,
-        cbar_kw={"drawedges": False, "shrink": 0.5, "pad": 0.05, "aspect": 10},
+        vmin=0,
+        cbar_kw={"drawedges": False, "shrink": 0.3, "pad": 0.02, "aspect": 10},
     )
+    cbar.set_label(f"{cbarlab}", rotation=90, va="bottom", labelpad=10)
 
     # texts = annotate_heatmap(im, valfmt="{x:.1f}")
     if annotate:
         texts = annotate_heatmap(im, valfmt="{x:.0f}", size=7)
+    if standard_colour:
+        set_label_colors_from_categories(
+            ax.get_xticklabels(),
+            get_pathway(version=defaultVersion),
+            colourDict["pathways"],
+        )
     # plt.figure(figsize=(10,6))
     ax.set_xlabel("substructure", labelpad=10)
     ax.set_ylabel("counts", labelpad=10)
     ax.set_title(title, loc="center", pad=20)
     fig.tight_layout()
-    # plt.show()
-    # for filename, using collection name is nice
-    # plt.savefig(f"{outfile_namer(name)}_heatmap.png")
-    return None
+    return fig
 
 
 def over_under_divide(fps: np.array, limit: int = 10, percentages: bool = True):
@@ -183,27 +220,33 @@ def over_under_divide(fps: np.array, limit: int = 10, percentages: bool = True):
 
 def fp_heatmap_accumulative(fp_arr: np.array, limit: int = 10, *args, **kwargs):
     under, over = over_under_divide(fp_arr, limit, percentages=True)
-    fp_heatmap(
+    hm = fp_heatmap(
         under,
         *args,
         percentages=True,
         top_acc_array=over,
         **kwargs,
     )
-    return None
+    return hm
 
 
 def main():
     print("hello")
     set_style()
+    ft = "svg"
     args = cli()
     fps = np.loadtxt(args.fingerprints, dtype=int, delimiter=",")
-    if args.name:
-        fp_name = args.name
-    else:
-        fp_name = args.fingerprints.split("/")[-1].split(".")[0]
-    # coco_name = fingerprintfile_coco.split("/")[-1].split(".")[0]
-    # bsf_name_coco = coco_name.replace("_noblock", '').replace(split("_")[-1]
+
+    fp_name = "_".join(args.fingerprints.split("/")[-1].split(".")[0].split("_")[1:])
+    set_name = args.fingerprints.split("/")[-1].split(".")[0].split("_")[0]
+
+    iwd = os.getcwd()
+    os.makedirs(f"{iwd}/heatmaps/{set_name}/{fp_name}", exist_ok=True)
+    os.chdir(f"{iwd}/heatmaps/{set_name}/{fp_name}")
+    fp_name = fp_name.replace("_", " ")
+
+    # save mean, std, median, etc of fps
+    write_stats(fps)
 
     substructure_names = get_names(version=defaultVersion)
     if fps.shape[1] != len(substructure_names):
@@ -218,24 +261,50 @@ def main():
             color_scheme="Greys",
             percentages=True,
             size=(15, 1),
+            standard_colour=True,
         )
-        plt.savefig(f"{outfile_namer(fp_name)}_heatmap.png")
+        plt.savefig(f"{fp_name}_heatmap.{ft}")
         return None
 
-    # coco_mean = fp_stats(coco, coco_name)
-    # zinc_mean = fp_stats(zinc, zinc_name)
-    # fp_plots(coco, coco_name)
-    # fp_plots(zinc, zinc_name)
-
-    fp_heatmap_accumulative(
+    hm = fp_heatmap_accumulative(
         fps,
         limit=10,
         title=f"Distribution of {fp_name} substructure counts",
         subslabels=substructure_names,
         color_scheme="GnBu",
+        standard_colour=True,
     )
-    plt.savefig(f"{outfile_namer(fp_name)}_heatmap.png")
+    savefig(hm, f"heatmap.{ft}")
 
+    if args.classes:
+        classes = np.loadtxt(args.classes, dtype="str", delimiter="\t", usecols=0)
+        classes[classes == "fatty_acid,isoprenoid"] = "isoprenoid"
+        classes = np.where(
+            np.core.defchararray.find(classes, ",") != -1, "multiple", classes
+        )
+        classes[classes == ""] = "None"
+
+        if len(classes) != fps.shape[0]:
+            logging.warning(
+                "classes file not same length as fingerprints file; will lead to errors"
+            )
+        for classif in np.unique(classes):
+            idx = np.where(classes == classif)
+            focus = fps[idx]
+            if not classif:
+                classif = "None"
+
+            hm = fp_heatmap_accumulative(
+                focus,
+                limit=10,
+                title=f"Distribution of {fp_name} substructure counts for {len(focus)} {classif.replace('_', ' ')} compounds",
+                subslabels=substructure_names,
+                color_scheme="GnBu",
+                standard_colour=True,
+            )
+            savefig(hm, f"{classif}_heatmap.{ft}")
+
+    os.chdir(iwd)
     # fp_means_plots(coco_mean, zinc_mean, outfile_namer(f"{coco_name}_{zinc_name}.svg"))
 
 
