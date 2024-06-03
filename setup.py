034b940802df690b9e02d9cce197e61b97f06a22
lucinamay <119406697+lucinamay@users.noreply.github.com>
2024-06-03 11:53:01 +0200

    [IMP] changed setup files to .toml based, in accordance with PEP621

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
 import setuptools
 
-setuptools.setup(
-    name="biosynfoni",
-    version="0.0.1",
-    #only one point for rdkit, numpy (e.g. make own smiles-> mol)
-    install_requires=[
-        "numpy",
-        "rdkit",
-        "tqdm",
-        ],
-    package_dir={"": "src"},
-    packages=["biosynfoni"],
-    python_requires=">=3.9",
-    entry_points={"console_scripts": ["biosynfoni = biosynfoni.concerto_fp:main"]}
-)
+setuptools.setup()
