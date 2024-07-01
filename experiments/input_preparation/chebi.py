7004afa88cb231b574182383739653c25ff10e74
lucinamay <119406697+lucinamay@users.noreply.github.com>
2024-07-01 23:21:05 +0200

    [IMP] improved output, cleaned code

--- experiments/input_preparation/chebi.py
+++ experiments/input_preparation/chebi.py
@@ -64,107 +64,59 @@ def get_properties(sdf: Chem.SDMolSupplier, query: list) -> pd.DataFrame:
             all_props[ind] = dict()
             for prop in query:
                 try:
-                    all_props[ind][cleanup(prop)] = mol.GetProp(prop)
+                    all_props[ind][prop] = mol.GetProp(prop)
                 except:
                     continue
-    return pd.DataFrame.from_dict(all_props, orient="index")
+    df = pd.DataFrame.from_dict(all_props, orient="index")
+    df["sdf_index"] = df.index
+    df.columns = [cleanup(i) for i in df.columns.to_list()]
+    return df
+
+
+def classifications(df, classification_path: str) -> pd.DataFrame:
+    """Returns dataframe with indexes of the sdf"""
+    classification = pd.read_csv(classification_path, sep="\t", index_col=0)
+    class_cols = classification.columns.to_list()
+    classification["chebi_id"] = classification.index
+    classification = classification.reset_index(drop=True)
+    classification.index = classification.index + 50000  # to avoid index overlap
+
+    # get idx, chebi and classification df
+    cl_df = pd.merge(df, classification, on="chebi_id", how="inner")
+    cl_df = cl_df.set_index("chebi_id")
+    # since flavonoids are just subset of (only) phenylpropanoids
+    # counts are contained in phenylpropanoids, so we drop flavonoid:
+    cl_df = cl_df[class_cols].drop(columns=["flavonoid"])
+    cl_df.columns = [convert_classes(i) for i in cl_df.columns.to_list()]
+
+    # merge columns with same name, with OR membership
+    return cl_df.groupby(level=0, axis=1).any()
 
 
 def main():
     sdf_path = "raw_data/ChEBI_complete_3star.sdf"
-    # classification_path = "chebi_classifications.tsv"
     classification_path = "raw_data/ChEBI_3star_classifications.tsv"
 
-    # get properties as dictionary
-    prop_query = [
-        "ChEBI ID",
-        # "Secondary ChEBI ID",
-        # "ChEBI Name",
-        "SMILES",
-        "InChI",
-        # "InChIKey",
-    ]
-
-    # make dataframe
+    # get properties and classifications
     sdf = Chem.SDMolSupplier(sdf_path)
-    df = get_properties(sdf, prop_query)
-    df["sdf_index"] = df.index
-
-    classification = pd.read_csv(classification_path, sep="\t", index_col=0)
-    classification["chebi_id"] = classification.index
-    classification.reset_index(inplace=True, drop=True)
-    classification.index = classification.index + 50000
-    print(classification.head())
-    classes = classification.columns.to_list()
-
-    df = pd.merge(df, classification, on="chebi_id", how="inner")
-    cl_df = df[classes].drop(columns="flavonoid")
-    # since flavonoids are just subset of (only) phenylpropanoids, counts are contained in phenylpropanoids
-    cl_df.columns = [convert_classes(i) for i in cl_df.columns.to_list()]
-    print(cl_df.head())
-    # merge columns with same name, with OR membership
-    cl_df = cl_df.groupby(level=0).any()
+    df = get_properties(sdf, ["ChEBI ID", "SMILES", "InChI"])
+    cl_df = classifications(df, classification_path)
     cl_df.to_csv("3star_converted_bool.tsv", sep="\t")  # membership table
 
-    # loop over cl_df rows:
+    # write classifications to csv
     with open("chebi_classes.csv", "w") as fo:
-        for row, data in cl_df.iterrows():
-            true_only = cl_df.columns[data.values]  # masks out False column values
-            tabbed = ";".join(true_only)
-            fo.write(f"{row},{tabbed}\n")
+        for chebi_id, data in cl_df.iterrows():
+            true_only = ";".join(cl_df.columns[data.values])  # keeps only True
+            true_only = ";".join(set(true_only.split(";")))  # removes duplicates
+            fo.write(f"{chebi_id},{true_only}\n")
 
-    sdf_indices = df.sdf_index.to_list()
+    # write sdf of classified mols
+    _is_classified = df.chebi_id.isin(cl_df.index.to_list())
+    sdf_indices = df[_is_classified].sdf_index.to_list()
     writer = Chem.SDWriter("chebi.sdf")
     for index in tqdm(sdf_indices, desc="writing sdf"):
-        writer.write(sdf[index])
+        writer.write(sdf[index])  # keeps properties
     writer.close()
-
-    # sdf_df["sdf_index"] = sdf_df.index
-    # sdf_df = sdf_df.set_index("chebi_id")
-
-    # read classifications
-    classification = pd.read_csv(classification_path, sep="\t", index_col=0)
-    classes = classification.columns.to_list()
-
-    # join mols with their classifications, keep only those that have classifications
-    # df = pd.concat(
-    #     [sdf_df, classification],
-    #     axis=1,
-    #     join="inner",
-    #     sort=False,
-    # )  # inner join on index
-    # print(df.head())
-    df = pd.merge(df, classification, on="chebi_id", how="inner", left_index=True)
-    print(df.head())
-    # df.to_csv("3star_class_props.tsv", sep="\t")
-    # df[classes].to_csv("3star_classifications.tsv", sep="\t")
-    # df = pd.read_csv("3star_classifications.tsv", sep="\t", index_col=0)
-
-    # make sdf of mols with classifications only
-    sdf_indices = df.index.to_list()
-    assert len(sdf_indices) == len(df[classes])
-    writer = Chem.SDWriter("chebi.sdf")
-    for index in tqdm(sdf_indices, desc="writing sdf"):
-
-        writer.write(sdf[index])
-    writer.close()
-
-    # make classification input ready
-    cl_df = df[classes].drop(
-        columns="flavonoid"
-    )  # since flavonoids are just subset of (only) phenylpropanoids, counts are contained in phenylpropanoids
-    cl_df.columns = [convert_classes(i) for i in cl_df.columns.to_list()]
-    print(cl_df.head())
-    # merge columns with same name, with OR membership
-    cl_df = cl_df.groupby(level=0).any()
-    cl_df.to_csv("3star_converted_bool.tsv", sep="\t")  # membership table
-
-    # loop over cl_df rows:
-    with open("chebi_classes.csv", "w") as fo:
-        for row, data in cl_df.iterrows():
-            true_only = cl_df.columns[data.values]  # masks out False column values
-            tabbed = ";".join(true_only)
-            fo.write(f"{row},{tabbed}\n")
 
     return None
 
