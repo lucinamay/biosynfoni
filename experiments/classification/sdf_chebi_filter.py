0b07b634b36a92d30de9818d958fb9381a54f2d8
lucinamay <119406697+lucinamay@users.noreply.github.com>
2024-07-01 20:20:35 +0200

    [IMP] improved output formatting and streamlined data prep

--- experiments/classification/sdf_chebi_filter.py
+++ experiments/classification/sdf_chebi_filter.py
@@ -12,12 +12,51 @@ from rdkit import RDLogger
 RDLogger.DisableLog("rdApp.*")
 
 
+def convert_classes(key: str) -> list:
+    cd = {
+        "alkaloid": ["alkaloid"],
+        "aminoacid": ["amino_acid"],
+        "aminofattyacid": ["amino_acid", "fatty_acid"],
+        "aminoglycan": ["amino_acid", "carbohydrate"],
+        "aminoglycoside": ["amino_acid", "carbohydrate"],
+        "carbohydrate": ["carbohydrate"],
+        "fattyacid": ["fatty_acid"],
+        "flavanglycoside": ["carbohydrate", "phenylpropanoid"],
+        "flavones": ["phenylpropanoid"],
+        "flavonoid": ["phenylpropanoid"],
+        "flavonoids": ["phenylpropanoid"],
+        "glycoalkaloid": ["alkaloid", "carbohydrate"],
+        "glycolipid": ["carbohydrate", "fatty_acid"],
+        "glycopeptide": ["amino_acid", "carbohydrate"],
+        "glycosaminoglycan": ["amino_acid", "carbohydrate"],
+        "glycosinolate": ["amino_acid", "carbohydrate"],
+        "glycosyloxyflavone": ["carbohydrate", "phenylpropanoid"],
+        "glycosyloxyisoflavone": ["carbohydrate", "phenylpropanoid"],
+        "isoprenoid": ["isoprenoid"],
+        "lipid": ["fatty_acid"],
+        "lipopeptide": ["amino_acid", "fatty_acid"],
+        "liposaccharide": ["carbohydrate", "fatty_acid"],
+        "peptide": ["amino_acid"],
+        "phenylpropanoid": ["phenylpropanoid"],
+        "polyketide": ["polyketide"],
+        "polysaccharide": ["carbohydrate"],
+        "saccharolipid": ["carbohydrate", "fatty_acid"],
+        "steroid": ["isoprenoid"],
+        "steroidalkaloid": ["alkaloid", "isoprenoid"],
+        "terpene": ["isoprenoid"],
+        "terpenealkaloid": ["alkaloid", "isoprenoid"],
+        "terpeneglycoside": ["carbohydrate", "isoprenoid"],
+        "terpenoid": ["isoprenoid"],
+    }
+    return ";".join(sorted(set(cd[key])))
+
+
 def cleanup(query: str) -> str:
     """Clean up query for use as column name."""
     return "_".join(query.lower().split(" "))
 
 
-def get_properties(sdf: Chem.SDMolSupplier, query: list) -> dict:
+def get_properties(sdf: Chem.SDMolSupplier, query: list) -> pd.DataFrame:
     """Get properties from sdf file."""
     all_props = dict()
     for ind, mol in tqdm(enumerate(sdf)):
@@ -28,83 +67,104 @@ def get_properties(sdf: Chem.SDMolSupplier, query: list) -> dict:
                     all_props[ind][cleanup(prop)] = mol.GetProp(prop)
                 except:
                     continue
-    return all_props
+    return pd.DataFrame.from_dict(all_props, orient="index")
 
 
 def main():
-    sdf_path = "/Users/lucina-may/thesis/chebiclasses/ChEBI_complete_3star.sdf"
-    classification_path = (
-        "/Users/lucina-may/thesis/chebiclasses/chebi_classifications.tsv"
-    )
+    sdf_path = "raw_data/ChEBI_complete_3star.sdf"
+    # classification_path = "chebi_classifications.tsv"
+    classification_path = "raw_data/ChEBI_3star_classifications.tsv"
 
-    # read sdf
-    sdf = Chem.SDMolSupplier(sdf_path)
     # get properties as dictionary
     prop_query = [
         "ChEBI ID",
-        "Secondary ChEBI ID",
-        "ChEBI Name",
+        # "Secondary ChEBI ID",
+        # "ChEBI Name",
         "SMILES",
         "InChI",
-        "InChIKey",
+        # "InChIKey",
     ]
-    all_props = get_properties(sdf, prop_query)
 
     # make dataframe
-    sdf_df = pd.DataFrame.from_dict(all_props, orient="index")
-    sdf_df["sdf_index"] = sdf_df.index
-    sdf_df.set_index("chebi_id", inplace=True)
+    sdf = Chem.SDMolSupplier(sdf_path)
+    df = get_properties(sdf, prop_query)
+    df["sdf_index"] = df.index
+
+    classification = pd.read_csv(classification_path, sep="\t", index_col=0)
+    classification["chebi_id"] = classification.index
+    classification.reset_index(inplace=True, drop=True)
+    classification.index = classification.index + 50000
+    print(classification.head())
+    classes = classification.columns.to_list()
+
+    df = pd.merge(df, classification, on="chebi_id", how="inner")
+    cl_df = df[classes].drop(columns="flavonoid")
+    # since flavonoids are just subset of (only) phenylpropanoids, counts are contained in phenylpropanoids
+    cl_df.columns = [convert_classes(i) for i in cl_df.columns.to_list()]
+    print(cl_df.head())
+    # merge columns with same name, with OR membership
+    cl_df = cl_df.groupby(level=0).any()
+    cl_df.to_csv("3star_converted_bool.tsv", sep="\t")  # membership table
+
+    # loop over cl_df rows:
+    with open("chebi_classes.csv", "w") as fo:
+        for row, data in cl_df.iterrows():
+            true_only = cl_df.columns[data.values]  # masks out False column values
+            tabbed = ";".join(true_only)
+            fo.write(f"{row},{tabbed}\n")
+
+    sdf_indices = df.sdf_index.to_list()
+    writer = Chem.SDWriter("chebi.sdf")
+    for index in tqdm(sdf_indices, desc="writing sdf"):
+        writer.write(sdf[index])
+    writer.close()
+
+    # sdf_df["sdf_index"] = sdf_df.index
+    # sdf_df = sdf_df.set_index("chebi_id")
 
     # read classifications
     classification = pd.read_csv(classification_path, sep="\t", index_col=0)
     classes = classification.columns.to_list()
 
-    # join dataframes on inner
-    df = pd.concat([sdf_df, classification], axis=1, join="inner")
+    # join mols with their classifications, keep only those that have classifications
+    # df = pd.concat(
+    #     [sdf_df, classification],
+    #     axis=1,
+    #     join="inner",
+    #     sort=False,
+    # )  # inner join on index
+    # print(df.head())
+    df = pd.merge(df, classification, on="chebi_id", how="inner", left_index=True)
+    print(df.head())
+    # df.to_csv("3star_class_props.tsv", sep="\t")
+    # df[classes].to_csv("3star_classifications.tsv", sep="\t")
+    # df = pd.read_csv("3star_classifications.tsv", sep="\t", index_col=0)
 
-    # write to file
-    df.to_csv("3star_class_props.tsv", sep="\t")
-    df[classes].to_csv("3star_classifications.tsv", sep="\t")
+    # make sdf of mols with classifications only
+    sdf_indices = df.index.to_list()
+    assert len(sdf_indices) == len(df[classes])
+    writer = Chem.SDWriter("chebi.sdf")
+    for index in tqdm(sdf_indices, desc="writing sdf"):
 
-    # read back in
-    df = pd.read_csv("3star_classifications.tsv", sep="\t", index_col=0)
-
-    # make reduced sdf
-    sdf_indexes = df.sdf_index.to_list()
-    assert len(sdf_indexes) == len(df[classes])
-    writer = Chem.SDWriter("3star_class.sdf")
-    for index in tqdm(sdf_indexes, desc="writing sdf"):
         writer.write(sdf[index])
     writer.close()
 
-    # read back in to check
-    sdf_reduced = Chem.SDMolSupplier("3star_class.sdf")
-
     # make classification input ready
-    cl_df = df[classes].drop(columns="flavonoid")
-    classes = cl_df.columns.to_list()
-    conversion = np.loadtxt("conversion.tsv", dtype=str, delimiter="\t")
-    conversion = {i[0]: i[1] for i in conversion}
-    converted_columns = [conversion[i] for i in classes]
-    cl_df.columns = converted_columns
-    # merge columns with same name, give true if any of the column values was true
-    cl_df = cl_df.groupby(level=0, axis=1).any()
-    cl_df.to_csv("3star_converted_bool.tsv", sep="\t")
+    cl_df = df[classes].drop(
+        columns="flavonoid"
+    )  # since flavonoids are just subset of (only) phenylpropanoids, counts are contained in phenylpropanoids
+    cl_df.columns = [convert_classes(i) for i in cl_df.columns.to_list()]
+    print(cl_df.head())
+    # merge columns with same name, with OR membership
+    cl_df = cl_df.groupby(level=0).any()
+    cl_df.to_csv("3star_converted_bool.tsv", sep="\t")  # membership table
 
     # loop over cl_df rows:
-    with open("3star_converted_onlytrue.tsv", "w") as fo:
+    with open("chebi_classes.csv", "w") as fo:
         for row, data in cl_df.iterrows():
-            true_only = cl_df.columns[data.values]
-            tabbed = "\t".join(true_only)
-            fo.write(f"{row}\t{tabbed}\n")
-
-    with open("3star_converted_onlytrue_commad.tsv", "w") as fo:
-        for row, data in cl_df.iterrows():
-            true_only = cl_df.columns[data.values]
-            commad = ",".join(true_only)
-            fo.write(f"{commad}\n")
-
-    # run biosynfoni
+            true_only = cl_df.columns[data.values]  # masks out False column values
+            tabbed = ";".join(true_only)
+            fo.write(f"{row},{tabbed}\n")
 
     return None
 
